d2d3ae3b5fef3dbe357e55aedbad3a2f2f10f061
David Pekar <dpekar@gmail.com>
2016-05-12 11:42:01 -0700

    Got bouncing and throwing working ok

--- Assets/Scripts/BagBehavior.cs
+++ Assets/Scripts/BagBehavior.cs
@@ -5,6 +5,14 @@ using System.Collections;
 public class BagBehavior : MonoBehaviour {
 
 	public float speed;
+	public float rollingDrag;
+	public float dragMagnitudeThreshold;
+
+	// Debugging
+	public float velocityMagnitude;
+	public float reverseForceMagnitude;
+
+
 	public float angle;
 	public Slider slider;
 
@@ -18,6 +26,15 @@ public class BagBehavior : MonoBehaviour {
 		notYetThrown = true;
 	}
 
+	void FixedUpdate() {
+		velocityMagnitude = bag.velocity.magnitude;
+		if (velocityMagnitude > 0.01/*dragMagnitudeThresholdLow*/ && velocityMagnitude < 1/*dragMagnitudeThresholdHigh*/) {			
+			Vector3 reverseForce = rollingDrag * -bag.velocity.normalized;
+			reverseForceMagnitude = reverseForce.magnitude;
+			bag.AddForce (reverseForce);
+		}
+	}
+
 	public void Grab() {
 		bag.useGravity = false;
 		bag.isKinematic = true;
@@ -29,14 +46,12 @@ public class BagBehavior : MonoBehaviour {
 			bag.isKinematic = false;
 			bag.transform.parent = null;
 
-			Debug.Log ("mass: " + bag.mass.ToString());
-
-			Vector3 movement = new Vector3 (0f, 100.0f, 100.0f);
-			//movement = movement * speed * slider.value;
-			Debug.Log ("relative force: " + movement.ToString());
+			Vector3 movement = new Vector3 ( 0f, 100.0f, 100.0f);
+			movement = movement * speed * slider.value;
 
 			bag.AddRelativeForce (movement);	
 			notYetThrown = false;
 		}
 	}
+		
 }
